0749369ebc5e33a84f93caad34cfac9c6b670893
Andrew Fechner <arfechner@gmail.com>
2017-06-07 23:37:17 -0400

    img switching out most of the time

--- zype.js
+++ zype.js
@@ -1,7 +1,9 @@
 const xhr = new XMLHttpRequest();
-const defaultImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/SMPTE_RP_219_color_bars.svg/1200px-SMPTE_RP_219_color_bars.svg.png"
+// const defaultImg = "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/SMPTE_RP_219_color_bars.svg/1200px-SMPTE_RP_219_color_bars.svg.png";
 
-let videos, thumbPromises, slideshow;
+const defaultImg = "./unicorn.jpeg"
+
+let videos, slideshow;
 
 const displayDefaultImg = (element) => element.src = "http://4.bp.blogspot.com/_soXI82GSn1A/TODxRXu8kUI/AAAAAAAADFM/JMsnljgBYxE/s1600/lookAroundYouScaryPicture.jpg";
 
@@ -35,40 +37,48 @@ const getVids = () => {
   });
 }
 
-const checkImg = (url) => {
-  return new Promise((resolve,reject) => {
-    xhr.open('GET', url);
-    xhr.onload = function() {
-      if (xhr.status === 200) {
-        resolve(url);
-      } else if (xhr.status === 404) {
-        resolve(defaultImg);
-      } else {
-        reject(Error(xhr.statusText));
-      }
-    };
-    xhr.onerror = () => reject(Error("Can't check img"));
-    xhr.send();
-  });
-}
+// const checkImg = (url) => {
+//   return new Promise((resolve,reject) => {
+//     xhr.open('GET', url);
+//     xhr.onload = function() {
+//       if (xhr.status === 200) {
+//         resolve(url);
+//       } else if (xhr.status === 404) {
+//         resolve(defaultImg);
+//       } else {
+//         reject(Error(xhr.statusText));
+//       }
+//     };
+//     xhr.onerror = () => reject(Error("Can't check img"));
+//     xhr.send();
+//   });
+// }
 
 getVids()
   .then(response => {
     videos = JSON.parse(response).response;
-    // let slideshow = $.map(videos,function(video){
-    //   return (`<div class="container"><div><img src="${video.thumbnails[4].url}" onerror="this.onerror=null;this.src="${defaultImg}"></div><div class="caption"><span></b>${video.title}</b></span></div></div>`);
-    // });
-    // console.log(slideshow);
-    // $(".slides").html(slideshow.join(''));
-    thumbPromises = videos.map(video => checkImg(video.thumbnails[4].url));
-  }, error => console.log('ERROR: ', error))
-  .then(data => Promise.all(thumbPromises))
-  .then(urls => {
-    slideshow = urls.map((url,i) => {
-      return (`<div class="container"><div><img src="${url}" onerror="this.onerror=null;this.src="${defaultImg}"></div><div class="caption"><span></b>${videos[i].title}</b></span></div></div>`);
+    let slideshow = $.map(videos,function(video){
+      return (`<div class="container"><div><img src="${video.thumbnails[4].url}"></div><div class="caption"><span></b>${video.title}</b></span></div></div>`);
     });
+    console.log(slideshow);
     $(".slides").html(slideshow.join(''));
+    // thumbPromises = videos.map(video => checkImg(video.thumbnails[4].url));
+  }, error => console.log('ERROR: ', error))
+  .then(data => {
+    $("div.slides").find("img").each(function() {
+      if ($(this)[0].naturalWidth === 120 && $(this)[0].naturalHeight === 90) {
+        $(this).attr("src", defaultImg);
+      }
+    })
   })
+  .then(data => $(".slides").css("display","block"))
+  // .then(data => Promise.all(thumbPromises))
+  // .then(urls => {
+  //   slideshow = urls.map((url,i) => {
+  //     return (`<div class="container"><div><img src="${url}" onerror="this.onerror=null;this.src="${defaultImg}"></div><div class="caption"><span></b>${videos[i].title}</b></span></div></div>`);
+  //   });
+  //   $(".slides").html(slideshow.join(''));
+  // })
   .catch(error => console.log('ERROR: ', error));
 
 
